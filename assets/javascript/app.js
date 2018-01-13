7b15bed031a0061a4de2b142312d8cef492964e0
Aditi Mishra <asmishra@Aditis-MacBook-Air.local>
2018-01-13 17:51:12 -0600

    starting logic

---
+++ assets/javascript/app.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+
+// global variables
+var player1;
+var player2;
+
+var name1 = "";
+var name2 = "";
+
+var yourName = "";
+
+var player1Choice = "";
+var player2Choices = "";
+
+var turn = 1;
+
+var wins1 = 0;
+var wins2 = 0;
+var losses1 = 0;
+var losses2 = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function to display name 
+function showName () {}
+
+// function to create RPS buttons
+function play () {}
+
+// on click function for RPS buttons
+
+// function to chat
+function chat () {}
+
+
+})
---
+++ assets/javascript/config.js
@@ -0,0 +1,10 @@
+// initialize Firebase
+var config = {
+    apiKey: "AIzaSyABtxVxWsLjjAiCQqGo88_tw86bw54i400",
+    authDomain: "rock-paper-scissors-27108.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-27108.firebaseio.com",
+    projectId: "rock-paper-scissors-27108",
+    storageBucket: "rock-paper-scissors-27108.appspot.com",
+    messagingSenderId: "659584618976"
+};
+  firebase.initializeApp(config);
